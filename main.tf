8e0ed2dc2016b7781036a343c2883fc13b14b683
Nicolas Cousin <contact@nicolas-cousin.com>
2023-05-17 19:06:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
-    organization = "example-organization"
+    organization = ${{ secrets.TF_ORGANIZATION }}
     
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
